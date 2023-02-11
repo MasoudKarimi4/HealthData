987182504772e0c08d0ee69721ac061b784c4021
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-02-11 17:56:19 -0500

    backend

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,20 @@
 from flask import Flask
+import datetime 
+
+x = datetime.datetime.now()
+
+# Init the flask app
+app = Flask(__name__)
+
+@app.route("/")
+def time():
+
+    return {
+        'Name': 'MK',
+        "Date": x
+    }
+
+if __name__ == '__main__':
+    app.run()
+
+    
